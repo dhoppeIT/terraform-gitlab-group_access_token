dhoppeIT/terraform-twingate-resource

chore: Initial commit

examples/default/providers.tf
@@ -1,0 +1,1 @@
+provider "twingate" {}

dhoppeIT/terraform-gitlab-group_access_token

examples/default/providers.tf
@@ -1,0 +1,1 @@
+provider "gitlab" {}